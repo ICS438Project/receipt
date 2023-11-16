1ec34b63812d4dd16ef7d9ae02ba1f7bfd4df7fc
Alexander Hung <hungalex@hawaii.edu>
2023-11-16 11:48:49 -1000

    Update receipt.py

--- src/receipt.py
+++ src/receipt.py
@@ -1,17 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# from openai import OpenAI
-# client = OpenAI()
-
-# response = client.completions.create(
-#     model="gpt-3.5-turbo",
-#     prompt="",
-#     temperature=1,
-#     max_tokens=256,
-#     top_p=1,
-#     frequency_penalty=0,
-#     presence_penalty=0
-# )
-
 st.write("Hello World")
