0cba20dc3f26cf365747592f94fc84ca4ae37bcc
Alexander Hung <hungalex@hawaii.edu>
2023-11-21 13:41:39 -1000

    Update receipt.py

--- src/receipt.py
+++ src/receipt.py
@@ -3,16 +3,16 @@
 import streamlit as st
 from langchain.llms import OpenAI
 
-data = {}
+data = []
 
 st.title('Start App')
 
 openai_api_key = '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'
 
-def generate_response(input_text):
+def generate_response(input_text, data):
   llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0.3, max_tokens=800, openai_api_key=openai_api_key, top_p=1, frequency_penalty=0, presence_penalty=0)
   st.info(llm(input_text))
-  data.update(llm(input_text))
+  data = [data, llm(input_text)]
 
 with st.form('my_form'):
   prompt = 'fill all info into this json data structure, and correct all the spelling'
@@ -42,6 +42,6 @@ with st.form('my_form'):
   text = st.text_area('Enter your scanned receipt:', '')
   submitted = st.form_submit_button('Submit')
   if submitted and openai_api_key.startswith('sk-'):
-    generate_response(prompt + text + structure)
+    generate_response(prompt + text + structure, data)
 
 st.write(data)
