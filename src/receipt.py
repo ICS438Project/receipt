9d649a90cf9deacf2ab6c1cc8dbaac951e591ca8
Alexander Hung <hungalex@hawaii.edu>
2023-11-21 10:44:34 -1000

    Update receipt.py

--- src/receipt.py
+++ src/receipt.py
@@ -5,7 +5,7 @@ from langchain.llms import OpenAI
 
 st.title('Start App')
 
-openai_api_key = st.sidebar.text_input('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')
+openai_api_key = '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'
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -14,7 +14,4 @@ def generate_response(input_text):
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+  generate_response(text)
