8139eae99a2416fa23640d3ad6e9dc6a08bce7a1
Alexander Hung <hungalex@hawaii.edu>
2023-11-21 10:38:31 -1000

    start app

--- src/receipt.py
+++ src/receipt.py
@@ -1,4 +1,18 @@
 import streamlit as st
-import streamlit.components.v1 as components
+from langchain.llms import OpenAI
 
-st.write("Hello World")
+st.title('Start App')
+
+openai_api_key = st.sidebar.text_input('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')
+
+def generate_response(input_text):
+  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  st.info(llm(input_text))
+
+with st.form('my_form'):
+  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  submitted = st.form_submit_button('Submit')
+  if not openai_api_key.startswith('sk-'):
+    st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted and openai_api_key.startswith('sk-'):
+    generate_response(text)
