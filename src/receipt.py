344d6504a10f77328749f2f3902c5efc87f5e360
Alexander Hung <hungalex@hawaii.edu>
2023-12-04 11:51:24 -1000

    dashboard

--- src/receipt.py
+++ src/receipt.py
@@ -5,29 +5,50 @@ from langchain.llms import OpenAI
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Sample data for demonstration
-data = {
-    'ReceiptID': [1, 2, 3, 4, 5],
-    'Vendor Category': ['Grocery', 'Health and Beauty', 'Clothing and Apparel', 'Grocery', 'Electronics and Appliances']
-}
+# Load your data into a DataFrame
+df = pd.read_csv('entities_database.csv')
 
-# Load data into a Pandas DataFrame
-df = pd.DataFrame(data)
+# Load your categories database into a DataFrame (assuming you have a CSV file with a column named 'Categories')
+categories_df = pd.read_csv('vender_categories _datebase.csv')
 
 # Set the title and description of the app
 st.title('Receipt Analytics Dashboard')
-st.write('Distribution of Receipts Across Vendor Categories')
+st.write('Distribution of Receipts Across Vendor and Item Categories')
 
-# Create a bar chart to display the distribution
-vendor_counts = df['Vendor Category'].value_counts()
-fig, ax = plt.subplots()
-ax.bar(vendor_counts.index, vendor_counts.values)
-ax.set_xlabel('Vendor Categories')
-ax.set_ylabel('Number of Receipts')
-plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+# Create filter widgets
+selected_vendor_category = st.selectbox('Select Vendor Category', df['Vendor Category'].unique())
+selected_item_category = st.selectbox('Select Item Category', df['Item Category'].unique())
 
-# Display the chart in Streamlit
+# Filter the DataFrame based on selected categories
+filtered_df = df[(df['Vendor Category'] == selected_vendor_category) & (df['Item Category'] == selected_item_category)]
+
+# Create a bar chart to display the distribution of Vendor Categories
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
+vendor_counts = filtered_df['Vendor Category'].value_counts()
+ax1.bar(vendor_counts.index, vendor_counts.values)
+ax1.set_xlabel('Vendor Categories')
+ax1.set_ylabel('Number of Receipts')
+ax1.set_xticklabels(vendor_counts.index, rotation=45)
+
+# Create a bar chart to display the distribution of Item Categories
+item_counts = filtered_df['Item Category'].value_counts()
+ax2.bar(item_counts.index, item_counts.values)
+ax2.set_xlabel('Item Categories')
+ax2.set_ylabel('Number of Items')
+ax2.set_xticklabels(item_counts.index, rotation=45)
+
+# Display the charts in Streamlit
 st.pyplot(fig)
+
+# Add a section to display the distribution of categories from the Categories database
+st.header('Distribution of Categories')
+category_counts = categories_df['Categories'].value_counts()
+fig_category, ax_category = plt.subplots(figsize=(10, 6))
+ax_category.bar(category_counts.index, category_counts.values)
+ax_category.set_xlabel('Categories')
+ax_category.set_ylabel('Count')
+ax_category.set_xticklabels(category_counts.index, rotation=45)
+st.pyplot(fig_category)
 
 _ = '''
 openai_api_key = ''
